f168f2ce37c7612464cc78dc2ec0fefd05539968
ameliadianmelinda <118956639+nashrilanam@users.noreply.github.com>
2024-02-26 15:00:43 +0700

    memperbaiki button profile

--- app/Views/user/profile.php
+++ app/Views/user/profile.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link href="<?= base_url(); ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/profile.css">
     <title>Profile</title>
 
     <style>
@@ -38,7 +39,7 @@
                             <div class="col-info">
                                 <div class="row mt-1">
                                     <div class="col-md d-flex justify-content-center">
-                                        <button type="button" onclick="redirectToPage('/user/album')">Album</button>
+                                        <button class="profile-batten-2 active-2" type="button" onclick="redirectToPage('/user/album')">Album</button>
                                         <button class="profile-batten-2 ms-2 me-2" type="button" onclick="redirectToPage('/')">Liked</button>
                                         <button class="profile-batten-2" type="button" onclick="redirectToPage('/')">MyPost</button>
                                     </div>
